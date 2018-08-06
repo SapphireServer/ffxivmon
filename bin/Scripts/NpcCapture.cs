cee8327941c25896ab0e9fb98ae3e1413ec5fd5d
goaaats <fakecake@icloud.com>
2018-08-06 14:27:39 +0200

    Add npc dump script

---
+++ bin/Scripts/NpcCapture.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+dynamic parsed = PacketObj;
+
+if(!Directory.Exists("CapturedNpcs"))
+{
+	Directory.CreateDirectory("CapturedNpcs");
+}
+
+if(Packet.Name == "InitZone")
+{
+	Debug.WriteLine("New zone id: " + parsed.zoneId);
+	DataStorage.Store("zoneId", parsed.zoneId);
+
+	if(!Directory.Exists(Path.Combine("CapturedNpcs", DataStorage.Get("zoneId").ToString())))
+		Directory.CreateDirectory(Path.Combine("CapturedNpcs", DataStorage.Get("zoneId").ToString()));
+}
+
+if(Packet.Name == "NpcSpawn")
+{
+	if(DataStorage.Get("zoneId") == null)
+	{
+		Debug.WriteLine("No zone id captured, not writing NPC...");
+		return;
+	}
+
+	var entId = BitConverter.ToUInt32(Packet.Data, 4);
+
+    Debug.WriteLine($"Spawn packet: {entId} Base: {parsed.bNPCBase}");
+
+    File.WriteAllBytes(Path.Combine(Environment.CurrentDirectory, "CapturedNpcs", DataStorage.Get("zoneId").ToString(), $"{entId}-{parsed.modelType}-{parsed.bNPCBase}.bin"), Packet.Data);
+}
