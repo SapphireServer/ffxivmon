d05745967257bbc5616cbc4cebb651a2f5b686c8
Maple <8961085+MapleHinata@users.noreply.github.com>
2023-03-22 04:43:26 -0300

    Optimize string creation on hot paths. (This stupid shit made capture loading much faster)

--- FFXIVMonReborn/Views/LogView.xaml.cs
+++ FFXIVMonReborn/Views/LogView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace FFXIVMonReborn
 {
@@ -20,17 +21,30 @@ namespace FFXIVMonReborn
 
         public void WriteLine(string line)
         {
-            string text = $"{line}\n";
-            InfoBox.AppendText(text);
-            Debug.WriteLine(text);
+            /*Concatenating line endings for the InfoBox.AppendText caused memory consumption and CPU usage to skyrocket
+             during Struct parsing thanks to the constant creation of new Strings.
+             Given how AppendText does this behind the scenes anyway, this was preferable to a StringBuilder.*/
+            InfoBox.Document.Blocks.Add(new Paragraph(new Run(line)));
+            Debug.WriteLine(line + "\n");
 
-            InfoBox.ScrollToEnd();
+            /*Unconditionally calling ScrollToEnd causes significant slowdown on AddPacketToListView, thanks to ScrollToEnd
+             calling UpdateLayout() regardless of visibility, recreating the layout of the RichTextBox on every state change, 
+             which happened to be every Packet addition.*/
+            if(IsVisible)
+                InfoBox.ScrollToEnd();
         }
 
         private void ScriptDebugView_OnClosing(object sender, CancelEventArgs e)
         {
             e.Cancel = true;
             this.Visibility = Visibility.Hidden;
+        }
+
+        private void LogView_OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            //e.NewValue is the new this.IsVisible
+            if ((bool) e.NewValue)
+                InfoBox.ScrollToEnd();
         }
     }
 }
